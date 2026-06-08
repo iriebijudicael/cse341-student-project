41885155bae546149fefc8aa4ff7536d4173b062
iriebijudicael <iriejudicaelelvis225@gmail.com>
2026-06-08 22:00:11 +0000

    Test commit on master branch

---
+++ src/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./config/db');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+  })
+  .use('/', require('./routes'));
+
+// Fallback Uncaught Exception Handler
+process.on('uncaughtException', (err, origin) => {
+  console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
+});
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on port ${port}`);
+  }
+});
